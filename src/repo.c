93672e5c9848979fc9cab10588b7fa019227b9e6
aestriplex <matteo.nicoli001@gmail.com>
2025-02-26 00:36:51 +0100

    added parse_repository function

--- src/repo.c
+++ src/repo.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <git2.h>
 
 static void print_commit_info(const git_commit *commit) {
@@ -15,40 +16,76 @@ static void print_commit_info(const git_commit *commit) {
 	printf("Message: %s\n\n", git_commit_message(commit));
 }
 
-const return_code_t get_repos_list(settings_t settings, str_t *list)
+static repository_t parse_repository(const char *line, ssize_t len)
+{
+	repository_t repo = { 0 };
+	
+	const char *bracket_open = strchr(line, '[');
+	if (bracket_open == NULL) {
+		repo.path = str_init(line, len);
+		repo.url = str_init_unbounded(0);
+		goto ret;
+	}
+	
+	const char *bracket_close = strchr(bracket_open, ']');
+	if (bracket_close == NULL) {
+		size_t path_len = bracket_open - line;
+		repo.path = str_init(line, path_len);
+		
+		size_t url_len = len - path_len - 1;
+		repo.url = str_init(bracket_open + 1, url_len);
+		goto ret;
+	}
+	
+	size_t path_len = bracket_open - line;
+	size_t url_len = bracket_close - bracket_open - 1;
+	
+	repo.path = str_init(line, path_len);
+	repo.url = str_init(bracket_open + 1, url_len);
+
+ret:
+	return repo;
+}
+
+static return_code_t get_repos_list(settings_t settings, repository_list_t *list)
 {
 	size_t len = 0;
-    ssize_t read;
+	ssize_t read;
 	char * line = NULL;
-	str_t current_line;
+	repository_list_t *current = list;
 	FILE *repos_list = fopen(settings.repos_path.val, "r");
 
 	if (!repos_list) { return INVALID_REPOS_LIST_PATH; }
 
 	while ((read = getline(&line, &len, repos_list)) != -1) {
-        current_line = str_init(line, read);
-    }
-
-    fclose(repos_list);
+		current->repository = parse_repository(line, read);
+		current->next = malloc(sizeof(repository_list_t));
+		if (!current->next) {
+			fprintf(stderr, "Cannot allocate memory for repository `%s`\n", line);
+			continue;
+		}
+		current = current->next;
+	}
+	fclose(repos_list);
 
 	return OK;
 }
 
-commit_history_t *get_commit_history(settings_t settings) {
+commit_history_t *get_commit_history(repository_t repo, settings_t settings) {
 	commit_history_t *history = NULL;
-	git_repository *repo = NULL;
+	git_repository *git_repo = NULL;
 	git_revwalk *walker = NULL;
 	git_oid oid;
 	git_commit *commit = NULL;
 
 	git_libgit2_init();
 
-	if (git_repository_open(&repo, "/Users/teo/Documents/GitHub/stats") != 0) {
+	if (git_repository_open(&git_repo, repo.path.val) != 0) {
 		fprintf(stderr, "Failed to open repository\n");
 		goto ret;
 	}
 
-	if (git_revwalk_new(&walker, repo) != 0) {
+	if (git_revwalk_new(&walker, git_repo) != 0) {
 		fprintf(stderr, "Failed to create revwalk\n");
 		goto cleanup;
 	}
@@ -57,7 +94,7 @@ commit_history_t *get_commit_history(settings_t settings) {
 	git_revwalk_sorting(walker, GIT_SORT_TIME);
 
 	while (git_revwalk_next(&oid, walker) == 0) {
-		if (git_commit_lookup(&commit, repo, &oid) == 0) {
+		if (git_commit_lookup(&commit, git_repo, &oid) == 0) {
 			print_commit_info(commit);
 			git_commit_free(commit);
 		}
@@ -67,7 +104,7 @@ commit_history_t *get_commit_history(settings_t settings) {
 	git_libgit2_shutdown();
 
 cleanup:
-	git_repository_free(repo);
+	git_repository_free(git_repo);
 ret:
 	return history;
 }
--- src/repo.h
+++ src/repo.h
@@ -2,11 +2,22 @@
 #define __REPO_H__
 
 #include "settings.h"
+#include "str.h"
 
 #include <stdint.h>
 
 #define AUTHORED 0x00
 #define CO_AUTHORED 0x02
+
+typedef struct {
+	str_t url;
+	str_t path;
+} repository_t;
+
+typedef struct _repository_list {
+	repository_t repository;
+	struct _repository_list *next;
+} repository_list_t;
 
 typedef struct {
 	char *hash;
@@ -18,6 +29,6 @@ typedef struct _commit_history {
 	struct _commit_history *parent;
 } commit_history_t;
 
-commit_history_t *get_commit_history(settings_t settings);
+commit_history_t *get_commit_history(repository_t repo, settings_t settings);
 
 #endif /* __REPO_H__ */
